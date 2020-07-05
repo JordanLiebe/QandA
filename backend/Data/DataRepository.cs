79e0ec91593f091f1d2294812842a0ede01b9190
Jordan M. Liebe <Jordan.Liebe@outlook.com>
2020-07-05 00:09:36 -0700

    Latest Update

---
+++ backend/Data/DataRepository.cs
@@ -0,0 +1,79 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using QandA.Data.Models;
+
+namespace QandA.Data
+{
+    public class DataRepository : IDataRepository
+    {
+        private readonly string _connectionString;
+
+        public AnswerGetResponse GetAnswer(int answerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QuestionGetSingleResponse GetQuestion(int questionId)
+        {
+            using(var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var question =
+                    connection.QueryFirstAsync<QuestionGetSingleResponse>(
+                        @"EXEC dbo.Question_GetSingle @QuestionId = @QuestionId",
+                        new { QuestionId = questionId }
+                    );
+
+                // TODO - Get the answers for the question
+
+                return question;
+            }
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetQuestions()
+        {
+            using(var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.Query<QuestionGetManyResponse>(
+                    @"EXEC dbo.Question_GetMany"
+                );
+            }
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                // TODO - execute Question_GetMany_BySearch stored procedure
+                return connection.Query<QuestionGetManyResponse>(
+                    @"EXEC dbo.Question_GetMany_BySearch @Search = @Search",
+                    new { Search = search }
+                );
+            }
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions()
+        {
+            using(var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.Query<QuestionGetManyResponse>(
+                    "EXEC dbo.Question_GetUnanswered"
+                );
+            }
+        }
+
+        public bool QuestionExists(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
